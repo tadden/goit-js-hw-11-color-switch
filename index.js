30f693ce1771f0c668b68bfe72d5c76333160dca
Ilja <tadden12@gmail.com>
2021-09-06 17:28:30 +0300

    finish

--- index.js
+++ index.js
@@ -7,3 +7,27 @@ const colors = [
   '#795548',
 ];
 
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('[data-action="start"]'),
+  stop: document.querySelector('[data-action="stop"]'),
+};
+
+let intervalId = null;
+
+function startClick() {
+   intervalId = setInterval(() => {
+    refs.start.disabled = true;
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    refs.body.style.backgroundColor = randomColor
+  }, 1000)
+};
+
+function stopClick() {
+  clearInterval(intervalId)
+  refs.start.disabled = false;
+}
+
+
+refs.start.addEventListener('click', startClick);
+refs.stop.addEventListener('click', stopClick)
